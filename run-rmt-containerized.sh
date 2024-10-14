e45028a578a8198d5c7216833d17aaf664e209bc
Benoit Loriot <benoit.loriot@gmail.com>
2024-10-14 17:01:44 +0200

    Add comment on how to automatically enable products

--- run-rmt-containerized.sh
+++ run-rmt-containerized.sh
@@ -35,6 +35,8 @@ podman run -d --rm --name rmt-nginx -p 80:80 -p 443:443 -v /srv/rmt/vhosts.d:/et
 MYSQL_HOST=`podman inspect -f "{{.NetworkSettings.IPAddress}}" rmt-mariadb`
 until podman exec -it rmt-mariadb mysql -u rmt -prmt -e "show databases">/dev/null; do echo "Waiting for mariadb to be available..." ; sleep 3 ; done
 podman run -d --rm --name rmt-server -p 4224:4224 -v /srv/rmt/storage:/var/lib/rmt -e MYSQL_HOST=${MYSQL_HOST} -e MYSQL_DATABASE=rmt -e MYSQL_USER=rmt -e MYSQL_PASSWORD=rmt -e SCC_USERNAME=${SCC_USERNAME} -e SCC_PASSWORD=${SCC_PASSWORD} ${REGISTRY_URL}/${RMT_SERVER_IMAGE}
+# it is possible to automatically enable products with additional parameter
+# -e SCC_PRODUCT_ENABLE=SLES/15.6/x86_64
 
 echo "Running containers"
 podman ps
